5d785b657d78d66eb9d460121d162fa6ea8c27e2
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-02-13 20:20:21 +0300

    Refactoring

--- Assets/Scripts/AttackSystem/Attacker.cs
+++ Assets/Scripts/AttackSystem/Attacker.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour
@@ -8,44 +9,77 @@ public class Attacker : MonoBehaviour
     [SerializeField] private float _frequency;
 
     private EnemyUnit _target;
+    private Coroutine _attackCoroutine;
     private float _attackTimer;
-
-    private void Update()
-    {
-        if (_target != null)
-        {
-            _attackTimer += Time.deltaTime;
-
-            if (_attackTimer >= _frequency)
-            {
-                AttackTarget();
-                _attackTimer = 0f;
-            }
-        }
-    }
 
     private void OnTriggerEnter(Collider target)
     {
-        if (((1 << target.gameObject.layer) & _targetMask) != 0)
+        int targetLayerMask = 1 << target.gameObject.layer;
+
+        if ((_targetMask & targetLayerMask) != 0)
             if (target.TryGetComponent(out EnemyUnit enemy) && _target == null)
                 _target = enemy;
+
+        if (_attackCoroutine == null && _target != null) 
+        {
+            _attackCoroutine = StartCoroutine(Attacking());
+            Debug.Log("Attacking Coroutine Started");
+        }
     }
 
     private void OnTriggerExit(Collider target)
     {
         if (target.TryGetComponent(out EnemyUnit enemy) && enemy == _target)
+            StopAttacking();
+    }
+
+    private IEnumerator Attacking()
+    {
+        while (enabled)
         {
-            _target = null;
-            _attackTimer = 0f;
+            if (_target != null && _target.gameObject != null) 
+            {
+                _attackTimer += Time.deltaTime;
+
+                if (_attackTimer >= _frequency)
+                {
+                    AttackTarget();
+                    _attackTimer = 0f;
+                }
+            }
+            else
+            {
+                StopAttacking();
+                yield break;
+            }
+
+            yield return null;
         }
     }
 
     private void AttackTarget()
     {
-        if (_target != null)
+        if (_target != null && _target.gameObject != null)
         {
             _target.TakeDamage(_damage);
             Debug.Log("Нанесено " + _damage + " урона " + _target.name);
         }
+        else
+        {
+            StopAttacking();
+        }
+    }
+
+    private void StopAttacking()
+    {
+        if (_attackCoroutine != null)
+        {
+            StopCoroutine(_attackCoroutine);
+            _attackCoroutine = null;
+            Debug.Log("Attacking Coroutine Stopped");
+        }
+
+        _target = null;
+        _attackTimer = 0f;
     }
 }
