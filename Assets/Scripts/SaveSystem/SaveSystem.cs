3ec2b93f287f149834d3ea685bc7d78db5465c1e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-02-17 14:48:07 +0500

    feat: add main menu and volume settings

---
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class SaveSystem : MonoBehaviour
+{
+    public abstract void Load();
+    public abstract void Save();
+}
---
+++ Assets/Scripts/SaveSystem/SaveSystemHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using YG;
+
+public class SaveSystemHandler : MonoBehaviour
+{
+    [SerializeField] private SaveSystem[] _saveSystems;
+
+    private void Start()
+    {
+       foreach (SaveSystem saveSystem in _saveSystems) 
+            saveSystem.Load();
+    }
+
+    private void OnDisable()
+    {
+        foreach (SaveSystem saveSystem in _saveSystems)
+            saveSystem.Save();
+    }
+}
