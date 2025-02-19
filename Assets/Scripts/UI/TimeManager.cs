e372bf4d5c4d27edd66e2cc81162b0222a834e91
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-02-19 11:32:34 +0500

    feat: add new assets

--- Assets/Scripts/UI/TimeManager.cs
+++ Assets/Scripts/UI/TimeManager.cs
@@ -5,13 +5,9 @@ public class TimeManager : MonoBehaviour
     private const float PauseScale = 0;
     private const float UnpauseScale = 1;
 
-    public void Pause()
-    {
+    public void Pause() => 
         Time.timeScale = PauseScale;
-    }
 
-    public void Unpause()
-    {
+    public void Unpause() => 
         Time.timeScale = UnpauseScale;
-    }
 }
