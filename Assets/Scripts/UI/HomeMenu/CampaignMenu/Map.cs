e4f9085b11cc92646f005673c80fd933c6e0600f
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-03-11 11:25:47 +0300

    refactor: add using

--- Assets/Scripts/UI/HomeMenu/CampaignMenu/Map.cs
+++ Assets/Scripts/UI/HomeMenu/CampaignMenu/Map.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UI.HomeMenu.CampaignMenu.Interfaces;
 
 namespace UI.HomeMenu.CampaignMenu
 {
     [CreateAssetMenu(fileName = "NewMap", menuName = "Scriptable Objects/Map")]
-    public class Map : ScriptableObject, Interfaces.IMapImageProvider
+
+    public class Map : ScriptableObject, IMapImageProvider
     {
         [SerializeField] private int _mapIndex;
         [SerializeField] private Sprite _mapImage;
