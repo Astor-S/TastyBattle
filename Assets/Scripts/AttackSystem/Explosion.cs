7deacdb0a6db643429085c62f489220713800f71
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-04-04 02:31:59 +0300

    feat: creation avocado ability

--- Assets/Scripts/AttackSystem/Explosion.cs
+++ Assets/Scripts/AttackSystem/Explosion.cs
@@ -1,19 +1,35 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace AttackSystem
 {
     public class Explosion : MonoBehaviour
     {
+        [SerializeField] private float _explosionForce = 0f;
+        [SerializeField] private int _maxColliders = 10;
+
+        private Collider[] _colliders;
+
+        private void OnEnable()
+        {
+            _colliders = new Collider[_maxColliders];
+        }
+
         public void Explode(float explosionRadius, float explosionDamage)
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-            
-            List<DamagableTarget> targets = colliders
-                .Select(collider => collider.GetComponent<DamagableTarget>())
-                .Where(target => target != null && target != this)
-                .ToList();
+            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, explosionRadius, _colliders);
+
+            List<DamagableTarget> targets = new();
+
+            for (int i = 0; i < numColliders; i++)
+            {
+                Collider collider = _colliders[i];
+                
+                if (collider.TryGetComponent(out DamagableTarget target) && target.transform != transform)
+                    targets.Add(target);
+                if (collider.TryGetComponent(out Rigidbody rb))
+                    rb.AddExplosionForce(_explosionForce, transform.position, explosionRadius, 1f, ForceMode.Impulse);
+            }
 
             foreach (DamagableTarget target in targets)
                 target.TakeDamage(explosionDamage);
--- Assets/Scripts/FactionalAbilities/Handlers/AvocadoAbilityHandler.cs
+++ Assets/Scripts/FactionalAbilities/Handlers/AvocadoAbilityHandler.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using AttackSystem;
 
 namespace FactionalAbilities.Handlers
 {
     public class AvocadoAbilityHandler : MonoBehaviour
     {
         [SerializeField] private AvocadoAbility _avocadoAbility;
+        [SerializeField] private DamagableTarget _damageTarget;
+        [SerializeField] private Explosion _explosion;
 
-        public AvocadoAbility AvocadoAbility => _avocadoAbility;
+        private void OnEnable()
+        {
+            if (_damageTarget != null)
+                _damageTarget.Dying += HandleDying;
+        }
+
+        private void OnDisable()
+        {
+            if (_damageTarget != null)
+                _damageTarget.Dying -= HandleDying; 
+        }
+
+        private void HandleDying(DamagableTarget target)
+        {
+            if (_avocadoAbility != null && _explosion != null)
+                _explosion.Explode(_avocadoAbility.ExplosionRadius, _avocadoAbility.ExplosionDamage);
+        }
     }
 }
