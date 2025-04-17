f604b81c74aaca3df89a19c3187e19c08d0cedc7
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-04-18 01:18:40 +0500

    feat: add description for races to level menu

--- Assets/Scripts/UI/HomeMenu/CampaignMenu/UnitMapView.cs
+++ Assets/Scripts/UI/HomeMenu/CampaignMenu/UnitMapView.cs
@@ -1,3 +1,8 @@
 using UnityEngine;
 
-public class UnitMapView : MonoBehaviour { }
+public class UnitMapView : MonoBehaviour
+{
+    [SerializeField] private string _description;
+
+    public string Description => _description;
+}
