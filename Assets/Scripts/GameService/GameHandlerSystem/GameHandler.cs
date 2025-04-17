88f42af8c756ca349de9fce047f07fafbea27f19
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-04-17 17:39:39 +0300

    feat: working on GameEnder

--- Assets/Scripts/GameService/GameHandlerSystem/GameHandler.cs
+++ Assets/Scripts/GameService/GameHandlerSystem/GameHandler.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
+using Buildings;
+using AttackSystem;
 
 namespace GameService.GameHandlerSystem
 {
     public class GameHandler : MonoBehaviour
     {
+        [SerializeField] private MainBuildingPresenter _playerBase;
         [SerializeField] private GameEnder _gameEnder;
         [SerializeField] private LevelCompletionHandler _levelCompletionHandler;
+
+        private void OnEnable()
+        {
+            if (_playerBase != null)
+                _playerBase.DamagableTarget.Dying += OnPlayerBaseDied;
+        }
+
+        private void OnDisable()
+        {
+            if (_playerBase != null)
+                _playerBase.DamagableTarget.Dying -= OnPlayerBaseDied;
+        }
 
         private void OnGameOver() =>
             _gameEnder.HandleGameOver();
@@ -13,5 +28,7 @@ namespace GameService.GameHandlerSystem
         private void OnCompleteLevel() =>
             _levelCompletionHandler.HandleLevelCompletion();
 
+        private void OnPlayerBaseDied(DamagableTarget target) =>
+            OnGameOver();
     }
 }
