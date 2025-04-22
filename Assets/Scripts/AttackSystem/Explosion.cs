0a325c5e88426642bb43621bb8c23cc5011abbf5
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-04-22 19:01:51 +0300

    feat: addition explosion particle effect

--- Assets/Scripts/AttackSystem/Explosion.cs
+++ Assets/Scripts/AttackSystem/Explosion.cs
@@ -1,11 +1,13 @@
+using System;
 using UnityEngine;
 
 namespace AttackSystem
 {
     public class Explosion : MonoBehaviour
     {
-        [SerializeField] private int _maxColliders = 10;
+        [SerializeField] private ParticleSystem _explosionParticleEffect;
         [SerializeField] private LayerMask _damageableLayers; 
+        [SerializeField] private int _maxColliders = 10;
 
         private Collider[] _colliders;
 
@@ -16,11 +18,29 @@ namespace AttackSystem
 
         public void Explode(float explosionRadius, float explosionDamage)
         {
+            PlayExplosionEffect();
+
             int numColliders = Physics.OverlapSphereNonAlloc(transform.position, explosionRadius, _colliders, _damageableLayers);
 
             for (int i = 0; i < numColliders; i++)
                 if (_colliders[i].TryGetComponent(out DamagableTarget target) && target.transform != transform)
                     target.TakeDamage(explosionDamage);          
+        }
+
+        private void PlayExplosionEffect()
+        {
+            if (_explosionParticleEffect == null)
+            {
+                Debug.LogWarning("Explosion Particle Effect Prefab не назначен!");
+                return;
+            }
+
+            ParticleSystem explosionInstance = Instantiate(_explosionParticleEffect, transform.position, Quaternion.identity);
+            explosionInstance.Play();
+
+            float duration = explosionInstance.main.duration;
+
+            Destroy(explosionInstance.gameObject, duration);
         }
     }
 }
