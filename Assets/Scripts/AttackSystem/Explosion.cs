08f95ba7944668afb37309d00938bfc1833d8aa5
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-04-22 03:30:00 +0300

    fix: avocado explosion fixes

--- Assets/Scripts/AttackSystem/Explosion.cs
+++ Assets/Scripts/AttackSystem/Explosion.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace AttackSystem
 {
     public class Explosion : MonoBehaviour
     {
-        [SerializeField] private float _explosionForce = 0f;
         [SerializeField] private int _maxColliders = 10;
+        [SerializeField] private LayerMask _damageableLayers; 
 
         private Collider[] _colliders;
 
@@ -17,23 +16,16 @@ namespace AttackSystem
 
         public void Explode(float explosionRadius, float explosionDamage)
         {
-            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, explosionRadius, _colliders);
-
-            List<DamagableTarget> targets = new();
+            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, explosionRadius, _colliders, _damageableLayers);
 
             for (int i = 0; i < numColliders; i++)
             {
                 Collider collider = _colliders[i];
-                
+
                 if (collider.TryGetComponent(out DamagableTarget target) && target.transform != transform)
-                    targets.Add(target);
-                if (collider.TryGetComponent(out Rigidbody rb))
-                    rb.AddExplosionForce(_explosionForce, transform.position, explosionRadius, 1f, ForceMode.Impulse);
+                    target.TakeDamage(explosionDamage);
             }
 
-            foreach (DamagableTarget target in targets)
-                target.TakeDamage(explosionDamage);
-            
             Debug.Log($"[AvocadoUnit] {gameObject.name} exploded, dealing {explosionDamage} damage in radius {explosionRadius}");
         }
     }
