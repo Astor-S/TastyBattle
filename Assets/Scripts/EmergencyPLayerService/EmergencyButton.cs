496d1059703d7a0e6754469898c025e230819f7a
Astor-S <ewg.sotnikow@yandex.ru>
2025-05-02 07:36:59 +0300

    feat: EmergencyPlayerService creating

--- Assets/Scripts/EmergencyPLayerService/EmergencyButton.cs
+++ Assets/Scripts/EmergencyPLayerService/EmergencyButton.cs
@@ -1,12 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
+using AttackSystem;
 
 namespace EmergencyPlayerService
 {
     public class EmergencyButton : MonoBehaviour
     {
-        public void OnButtonClick()
-        {
+        [SerializeField] private Button _emergencyButton;
+        [SerializeField] private DamagableTarget _target;
 
+        private void Start()
+        {
+            _emergencyButton.interactable = false;
         }
+
+        private void OnEnable()
+        {
+            if (_target != null)
+                _target.HalfHP += OnHalfHPReached;   
+        }
+
+        private void OnDisable()
+        {
+            if (_target != null)
+                _target.HalfHP -= OnHalfHPReached;
+        }
+
+        public void OnButtonClick() =>
+            gameObject.SetActive(false);
+
+        private void OnHalfHPReached() =>
+            _emergencyButton.interactable = true;
     }
 }
