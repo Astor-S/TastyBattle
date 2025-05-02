e9bcef498741f19722e197e81d975bbbdba6cd7f
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-05-02 15:34:29 +0500

    feat: delete YG2

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -9,17 +9,11 @@ namespace Units
         private const string Enemy = nameof(Enemy);
         private const string Player = nameof(Player);
 
-        private float _currentMovementSpeed;
-        private float _currentAttackSpeed;
-
         public Faction Faction { get; }
         public BattleRole BattleRole { get; }
         public UnitSetup Stats { get; }
         public DamagableTarget EnemyBase { get; }
         public LayerMask OwnerMask { get; }
-
-        public float CurrentMovementSpeed => _currentMovementSpeed;
-        public float CurrentAttackSpeed => _currentAttackSpeed;
 
         public Unit(UnitSetup setup, DamagableTarget enemyBase)
         {
@@ -32,15 +26,6 @@ namespace Units
                 OwnerMask = LayerMask.NameToLayer(Enemy);
             else
                 OwnerMask = LayerMask.NameToLayer(Player);
-
-            _currentMovementSpeed = Stats.MovementSpeed;
-            _currentAttackSpeed = Stats.AttackSpeed;
         }
-
-        public void SetMovementSpeed(float newSpeed) =>
-            _currentMovementSpeed = newSpeed;
-
-        public void SetAttackSpeed(float newSpeed) =>
-            _currentAttackSpeed = newSpeed;
     }
 }
