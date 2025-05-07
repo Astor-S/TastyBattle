2574e3fc4a39ff9a30a939f9b4ec770601c49a82
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-05-07 13:35:00 +0500

    refactor: add start volume for audio

--- Assets/Scripts/SaveSystem/SavesYG.cs
+++ Assets/Scripts/SaveSystem/SavesYG.cs
@@ -3,11 +3,11 @@ namespace YG
     public partial class SavesYG
     {        
         //Audio saves
-        public float MusicVolume;
-        public float SoundVolume;
+        public float MusicVolume = 0.3f;
+        public float SoundVolume = 0.3f;
 
-        public bool IsMusicOn;
-        public bool IsSoundOn;
+        public bool IsMusicOn = true;
+        public bool IsSoundOn = true;
 
         //Score
         public int Score;
