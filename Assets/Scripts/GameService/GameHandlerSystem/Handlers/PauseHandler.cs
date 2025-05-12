a972ff6918ca25bc9acbb6a62e976197b52ea4be
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-05-12 19:43:38 +0300

    fix: button in pause panel fixes

--- Assets/Scripts/GameService/GameHandlerSystem/Handlers/PauseHandler.cs
+++ Assets/Scripts/GameService/GameHandlerSystem/Handlers/PauseHandler.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameService.GameHandlerSystem.Handlers
 {
     public class PauseHandler : MonoBehaviour
     {
         private readonly float _pauseDelayForSeconds = 0.1f;
-        
+
+        [SerializeField] private Button _homeButtonInPausePanel;
+        [SerializeField] private Button _restartButtonInPausePanel;
+
         private WaitForSeconds _waitPauseDelayForSeconds;
 
         private void Awake()
         {
             _waitPauseDelayForSeconds = new WaitForSeconds(_pauseDelayForSeconds);
+        }
+
+        private void OnEnable()
+        {
+            if(_homeButtonInPausePanel != null)
+               _homeButtonInPausePanel.onClick.AddListener(ContinueGame);
+
+            if (_restartButtonInPausePanel != null)
+                _restartButtonInPausePanel.onClick.AddListener(ContinueGame);
+        }
+
+        private void OnDisable()
+        {
+            if (_homeButtonInPausePanel != null)
+                _homeButtonInPausePanel.onClick.RemoveListener(ContinueGame);
+
+            if (_restartButtonInPausePanel != null)
+                _restartButtonInPausePanel.onClick.RemoveListener(ContinueGame);
         }
 
         public IEnumerator PauseGameDelayed()
