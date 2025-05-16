0ef3265733787c5737a240af7cf6e1e2b906bc34
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-05-16 11:11:59 +0300

    feat: award screen completing

--- Assets/Scripts/UI/Screens/AwardScreen.cs
+++ Assets/Scripts/UI/Screens/AwardScreen.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
+using YG;
 
 namespace UI.Screens
 {
     public class AwardScreen : MonoBehaviour
     {
-        //[SerializeField] private Skins _skinToAdd;
-
         private void Start()
         {
             gameObject.SetActive(false);
@@ -13,16 +12,16 @@ namespace UI.Screens
 
         public void GiveReward()
         {
-            //if (_savesYG.ownedSkins.Contains(_skinToAdd) == false)
-            //{
-            //    gameObject.SetActive(true);
-            //    _savesYG.ownedSkins.Add(_skinToAdd);
-            //    YandexGame.SaveProgress();
-            //}
-            //else
-            //{
-            //    gameObject.SetActive(false);
-            //}
+            if(YG2.saves.isMushroomCampaignCompleted == false)
+            {
+                gameObject.SetActive(true);
+                YG2.saves.isMushroomCampaignCompleted = true;
+                YG2.SaveProgress();
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
