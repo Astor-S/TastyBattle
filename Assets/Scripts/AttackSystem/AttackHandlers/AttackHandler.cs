888ba02e3080547268036eb69f47e8624ba4e4c3
kuRomek <qustes@gmail.com>
2025-05-21 21:32:03 +0400

    fix: fix attacking (i hope)

--- Assets/Scripts/AttackSystem/AttackHandlers/AttackHandler.cs
+++ Assets/Scripts/AttackSystem/AttackHandlers/AttackHandler.cs
@@ -66,7 +66,7 @@ namespace AttackSystem.AttackHandlers
                     StartAttack();
                     _isAttacking = true;
                 }
-                else if (ReadyToAttack == false && _isAttacking)
+                else if (ReadyToAttack == false)
                 {
                     StopAttack();
                     _isAttacking = false;
