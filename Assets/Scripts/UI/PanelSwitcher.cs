8e82d6fa20c9fd3d414d9c796bcb76dbfb8f9a5e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-05-23 03:27:08 +0500

    fix: ui fix

--- Assets/Scripts/UI/PanelSwitcher.cs
+++ Assets/Scripts/UI/PanelSwitcher.cs
@@ -1,8 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class PanelSwitcher : MonoBehaviour, ISwitchable
 {
     private Transform _closingPanel;
+    private WaitForSeconds _delay;
+    private float _delayTime = 1f;
+
+    private void OnEnable() => 
+        _delay = new WaitForSeconds(_delayTime);
 
     public void Open(Transform openingPanel)
     {
@@ -11,5 +17,20 @@ public class PanelSwitcher : MonoBehaviour, ISwitchable
 
         _closingPanel.gameObject.SetActive(false);
         openingPanel.gameObject.SetActive(true);
+    }
+
+    public void OpenDelayed(Transform openingPanel) => 
+        StartCoroutine(Delay(openingPanel));
+
+    private IEnumerator Delay(Transform openingPanel)
+    {       
+        if (_closingPanel == null)
+            _closingPanel = transform;
+
+        openingPanel.gameObject.SetActive(true);
+
+        yield return _delay;
+
+        _closingPanel.gameObject.SetActive(false);
     }
 }
