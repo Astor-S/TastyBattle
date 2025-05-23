4f494dcee385f0544760754884342c34c98b3e1f
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-05-23 13:33:29 +0500

    fix: ui active form

--- Assets/Scripts/UI/PanelSwitcher.cs
+++ Assets/Scripts/UI/PanelSwitcher.cs
@@ -5,9 +5,9 @@ public class PanelSwitcher : MonoBehaviour, ISwitchable
 {
     private Transform _closingPanel;
     private WaitForSeconds _delay;
-    private float _delayTime = 1f;
+    private float _delayTime = 0.75f;
 
-    private void OnEnable() => 
+    private void OnEnable() =>
         _delay = new WaitForSeconds(_delayTime);
 
     public void Open(Transform openingPanel)
@@ -19,11 +19,11 @@ public class PanelSwitcher : MonoBehaviour, ISwitchable
         openingPanel.gameObject.SetActive(true);
     }
 
-    public void OpenDelayed(Transform openingPanel) => 
+    public void OpenDelayed(Transform openingPanel) =>
         StartCoroutine(Delay(openingPanel));
 
     private IEnumerator Delay(Transform openingPanel)
-    {       
+    {
         if (_closingPanel == null)
             _closingPanel = transform;
 
@@ -32,5 +32,6 @@ public class PanelSwitcher : MonoBehaviour, ISwitchable
         yield return _delay;
 
         _closingPanel.gameObject.SetActive(false);
+        openingPanel.gameObject.SetActive(true);
     }
 }
