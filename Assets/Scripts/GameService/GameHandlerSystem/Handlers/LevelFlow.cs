6da650ac32ec370bbc8a4d650b1b87a65abf2312
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-06-02 20:12:54 +0300

    refactor: renaiming

--- Assets/Scripts/GameService/GameHandlerSystem/Handlers/LevelFlow.cs
+++ Assets/Scripts/GameService/GameHandlerSystem/Handlers/LevelFlow.cs
@@ -4,7 +4,7 @@ using Buildings;
 
 namespace GameService.GameHandlerSystem.Handlers
 {
-    public class GameHandler : MonoBehaviour
+    public class LevelFlow : MonoBehaviour
     {
         [SerializeField] private MainBuildingPresenter _playerBase;
         [SerializeField] private MainBuildingPresenter _enemyBase;
