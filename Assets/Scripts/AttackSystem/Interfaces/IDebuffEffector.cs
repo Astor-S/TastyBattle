9f1037123b88905c81baf2598f94a2fa22b74617
kuRomek <qustes@gmail.com>
2025-06-03 19:02:21 +0400

    refactor: follow DRY and KISS for attack handlers

--- Assets/Scripts/AttackSystem/Interfaces/IDebuffEffector.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace AttackSystem.Interfaces
-{
-    public interface IDebuffEffector
-    {
-        ParticleSystem PlayDebuffParticleEffect(Transform target);
-    }
-}
--- Assets/Scripts/FactionalAbilities/Handlers/IceCreamAbilityHandler.cs
+++ Assets/Scripts/FactionalAbilities/Handlers/IceCreamAbilityHandler.cs
@@ -1,11 +1,76 @@
+using FactionalAbilities;
+using FactionalAbilities.Handlers.Debuffs;
+using Units;
 using UnityEngine;
 
-namespace FactionalAbilities.Handlers
+namespace AttackSystem.AttackHandlers
 {
     public class IceCreamAbilityHandler : MonoBehaviour
     {
+        [SerializeField] private AttackHandler _attackHandler;
+        [SerializeField] private ParticleSystem _freezeParticleEffectPrefab;
         [SerializeField] private IceCreamAbility _iceCreamAbility;
 
-        public IceCreamAbility IceCreamAbility => _iceCreamAbility;
+        private void OnEnable()
+        {
+            _attackHandler.HitExcecuting += ApplyDebuff;
+        }
+
+        private void OnDisable()
+        {
+            _attackHandler.HitExcecuting -= ApplyDebuff;
+        }
+
+        public void ApplyDebuff()
+        {
+            if (_attackHandler.AttackedTarget == null || _attackHandler.AttackedTarget.gameObject.activeInHierarchy == false)
+                return;
+
+            if (_attackHandler.AttackedTarget.TryGetComponent<UnitPresenter>(out var unitPresenter) == false)
+                return;
+                        
+            if (_attackHandler.AttackedTarget.TryGetComponent<FreezeHandler>(out var freezeHandler) == false)
+            {
+                ParticleSystem freezeParticleEffectInstance =
+                    PlayDebuffParticleEffect(_attackHandler.AttackedTarget.transform);
+
+                freezeHandler = _attackHandler.AttackedTarget.gameObject.AddComponent<FreezeHandler>();
+                freezeHandler.Initialize(
+                    unitPresenter,
+                    _iceCreamAbility.FreezePercentage,
+                    _iceCreamAbility.MaxFreezePercentage,
+                    _iceCreamAbility.SlowDecreaseRate,
+                    freezeParticleEffectInstance);
+            }
+            else
+            {
+                freezeHandler.ApplySlow(_iceCreamAbility.FreezePercentage);
+            }
+        }
+
+        public ParticleSystem PlayDebuffParticleEffect(Transform target)
+        {
+            float quaternionX = -90f;
+            float quaternionY = 0f;
+            float quaternionZ = 0f;
+
+            if (_freezeParticleEffectPrefab != null)
+            {
+                ParticleSystem freezeParticleEffect =
+                    Instantiate(
+                        _freezeParticleEffectPrefab,
+                        target.position,
+                        Quaternion.Euler(quaternionX, quaternionY, quaternionZ),
+                        target);
+
+                ParticleSystem.MainModule mainModule = freezeParticleEffect.main;
+                mainModule.stopAction = ParticleSystemStopAction.Destroy;
+                freezeParticleEffect.Play();
+
+                return freezeParticleEffect;
+            }
+
+            return null;
+        }
     }
 }
