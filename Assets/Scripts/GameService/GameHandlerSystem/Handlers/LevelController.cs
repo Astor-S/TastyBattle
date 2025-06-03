855696ea6e9dd402fd01daf98ac277f7e54b2b52
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-06-03 11:42:42 +0300

    style: rename class

--- Assets/Scripts/GameService/GameHandlerSystem/Handlers/LevelController.cs
+++ Assets/Scripts/GameService/GameHandlerSystem/Handlers/LevelController.cs
@@ -4,7 +4,7 @@ using Buildings;
 
 namespace GameService.GameHandlerSystem.Handlers
 {
-    public class LevelFlow : MonoBehaviour
+    public class LevelController : MonoBehaviour
     {
         [SerializeField] private MainBuildingPresenter _playerBase;
         [SerializeField] private MainBuildingPresenter _enemyBase;
